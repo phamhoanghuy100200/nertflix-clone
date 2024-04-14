eeea39ed69be10978dc5fd15cbdcef2ad1af863d
Huy Pham <huyhoangpham100200@gmail.com>
2024-04-14 13:44:52 +0700

    fix

--- app/actions/getMovieRandom.ts
+++ app/actions/getMovieRandom.ts
@@ -2,8 +2,8 @@ import prisma from '@/app/lib/prismadb';
 
 const getMovieRandom = async () => {
     try {
-        const moiveCount = await prisma.movie.count();
-        const randomIndex = Math.floor(Math.random() * moiveCount);
+        // const moiveCount = await prisma.movie.count();
+        // const randomIndex = Math.floor(Math.random() * moiveCount);
         const randomMovies = await prisma.movie.findFirst()
         return randomMovies;
     } catch (error: any) {
